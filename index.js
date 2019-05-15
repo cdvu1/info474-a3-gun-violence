db6335dcb3cf563191c5f37f82a0fae699ec2fd4
Liam Brozik <lbrozik@uw.edu>
2019-05-14 20:50:38 -0700

    Finished first chart with transitions and style

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
-var margin = { top: 20, right: 20, bottom: 30, left: 50 },
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+var margin = { top: 40, right: 100, bottom: 50, left: 100 },
+  width = 1020 - margin.left - margin.right,
+  height = 531.25 - margin.top - margin.bottom;
 
 // parse year
 //var parseYear = d3.time.format("%Y").parse;
@@ -38,17 +38,134 @@ var div = d3
   .attr("class", "tooltip")
   .style("opacity", 0);
 
-// Get the data
-d3.csv("clean_data.csv", function(error, data) {
-  if (error) throw error;
+d3.csv("gun_violence_data.csv", function(error, data) {
+  var ag_data = agregateData(data);
+  // Scale the range of the data
 
-  // format the data
-  data.forEach(function(d) {
-    d.Year = +d.Year;
-    d.Total_Shootings = +d.Total_Shootings;
+  // draw the initial visualization
+  drawVis_1(ag_data);
+
+  // Adding the dropdown
+  var select = d3.select("#weapon-type");
+
+  select.on("change", function(d) {
+    var value = d3.select(this).property("value");
+    filterType(value, data);
   });
 
-  // Scale the range of the data
+  select
+    .append("option")
+    .attr("value", "All")
+    .text("All");
+
+  select
+    .selectAll("option")
+    .data(
+      d3
+        .map(data, function(d) {
+          return d["Firearm Type"];
+        })
+        .keys()
+    )
+    .enter()
+    .append("option")
+    .attr("value", function(d) {
+      return d;
+    })
+    .text(function(d) {
+      return d;
+    });
+});
+
+agregateData = data => {
+  data = data.filter(function(d) {
+    return +d.Date.split("/")[2] >= 2008;
+  });
+  data = data.filter(function(d) {
+    return +d.Date.split("/")[2] <= 2018;
+  });
+  data = d3
+    .nest()
+    .key(function(d) {
+      // Summing by firearm type
+      return d.Date.split("/")[2];
+    })
+    .rollup(function(leaves) {
+      return leaves.length;
+    })
+    .entries(data)
+    .map(function(d) {
+      return { Year: d.key, Total_Shootings: d.value };
+    });
+  return data;
+};
+
+filterType = (value, data) => {
+  if (value == "All") {
+    data = data.filter(function(d) {
+      return d["Firearm Type"] != "";
+    });
+  } else {
+    data = data.filter(function(d) {
+      return d["Firearm Type"] == value;
+    });
+  }
+  var ag_data = agregateData(data);
+
+  // UPDATE THE VISUALIZATION
+  y.domain([
+    0,
+    d3.max(ag_data, function(d) {
+      return d.Total_Shootings;
+    })
+  ]);
+
+  // Make the changes
+  svg
+    .select(".line")
+    .transition() // change the line
+    .duration(750)
+    .attr("d", valueline(ag_data));
+
+  svg
+    .select(".y.axis")
+    .transition() // change the y axis
+    .duration(750)
+    .call(d3.axisLeft(y).ticks(4));
+
+  svg
+    .selectAll("circle")
+    .data(ag_data)
+    .transition()
+    .duration(750)
+    .attr("cx", function(d) {
+      return x(d.Year);
+    })
+    .attr("cy", function(d) {
+      return y(d.Total_Shootings);
+    });
+
+  //Enter new circles
+  d3.selectAll("circle")
+    .data(ag_data)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) {
+      return x(d.Year);
+    })
+    .attr("cy", function(d) {
+      return y(d.Total_Shootings);
+    })
+    .attr("r", 3);
+
+  // Remove old
+  d3.selectAll("circle")
+    .data(ag_data)
+    .exit()
+    .remove();
+};
+
+drawVis_1 = data => {
   x.domain(
     d3.extent(data, function(d) {
       return d.Year;
@@ -109,8 +226,37 @@ d3.csv("clean_data.csv", function(error, data) {
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .attr("class", "x axis")
+    .call(
+      d3.axisBottom(x).tickFormat(function(n) {
+        return n * 1;
+      })
+    );
 
+  // X Axis Label
+  svg
+    .append("text")
+    .attr(
+      "transform",
+      "translate(" + width / 2 + " ," + (height + margin.top + 0) + ")"
+    )
+    .style("text-anchor", "middle")
+    .style("font-size", "12px")
+    .text("Year");
   // Add the Y Axis
-  svg.append("g").call(d3.axisLeft(y));
-});
+  svg
+    .append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(y).ticks(4));
+
+  // Y Axis Label
+  svg
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left / 2)
+    .attr("x", 0 - height / 2)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-size", "12px")
+    .text("Number of Shootings");
+};
