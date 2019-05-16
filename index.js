86a93d461e433a00a989b00f3d15f5d8baf92aae
UWParlay <UWParlay>
2019-05-15 20:32:33 -0700

    working version plot

--- index.js
+++ index.js
@@ -2,30 +2,23 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
 width = 1000 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
-console.log(margin);
 
-// set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+// // set the ranges
+// var x = d3.scaleTime().range([0, width]);
+// var y = d3.scaleLinear().range([height, 0]);
 
-// define the line
-var valueline = d3.line()
-.x(function(d) { return x(d.Year); })
-.y(function(d) { return y(d.Total_Shootings); });
+// // define the line
+// var valueline = d3.line()
+// .x(function(d) { return x(d.Year); })
+// .y(function(d) { return y(d.Total_Shootings); });
 
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")")
-  
+
 function drawGraph(xText, yText) {
-	$('svg').remove();
-	
+  $('svg').remove();
+  
   // setup x 
   var xValue = function(d) { return d[xText];}, // data -> value
   xScale = d3.scaleLinear().range([0, width]), // value -> display
@@ -46,25 +39,28 @@ function drawGraph(xText, yText) {
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
   // Get the data
   d3.csv("gun_violence_data.csv", function(error, data) {
     if (error) throw error;
 
-     // change string (from CSV) into number format
+   // change string (from CSV) into number format
 	  data.forEach(function(d) {
       d[yText] = +d[yText];
       if (d[xText] == "N") {
         d[xText] = 0
-      } else {
+      } else if (d[xText] == "Y"){
         d[xText] = 1
+      } else if (d[xText] == "Unknown") {
+        d[xText] = 2
+      } else {
+        d[xText] = 3
       }
     });
-  
+    
       // don't want dots overlapping axis, so add in buffer to data domain
       xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
       yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
-  
+
       // x-axis
       svg.append("g")
         .attr("transform", "translate(0," + height + ")")
@@ -75,7 +71,9 @@ function drawGraph(xText, yText) {
                              (height + margin.top + 10) + ")")
         .style("text-anchor", "middle")
         .text(xText);
-  
+
+      
+        
       // y-axis
       svg.append("g")
         .call(yAxis)
@@ -90,6 +88,7 @@ function drawGraph(xText, yText) {
       svg.selectAll(".dot")
         .data(data)
       .enter().append("circle")
+      .transition().duration(750)
         .attr("class", "dot")
         .attr("r", 3.5)
         .attr("cx", xMap)
